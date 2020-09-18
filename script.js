a16d75c2d5098c04d0010cd54ec40d79234b0350
MariskaS85 <mcfsnijdewind@gmail.com>
2020-09-18 15:08:07 +0000

    images modal

--- script.js
+++ script.js
@@ -7,3 +7,29 @@ function scrollFunction() {
     document.getElementById("header").style.fontSize = "90px";
   }
 }
+$(function() {
+		$('.sfeer').on('click', function() {
+			$('.imagepreview').attr('src', $(this).find('img').attr('src'));
+			$('#imagemodal').modal('show');   
+		});		
+});
+
+
+function zoomIn(event) {
+  var pre = document.getElementById("preview");
+  pre.style.visibility = "visible";
+  if ($('#menukaart').is(':hover')) {
+        var img = document.getElementById("zoom1");
+		pre.style.backgroundImage = "url('assets/images/FRISZ_Menu_A3_2020-2-1.png')";
+    }
+  if ($('#menukaart').is(':hover')){
+		var img = document.getElementById("zoom2");
+		pre.style.backgroundImage = "url('assets/images/FRISZ_Menu_A3_2020-2-1.png')";
+  } 
+
+}
+
+function zoomOut() {
+  var pre = document.getElementById("preview");
+  pre.style.visibility = "hidden";
+}
